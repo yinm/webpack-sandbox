2c22b99b5f6f5d47ddc4c5fdf3d58092cd74c410
yinm <yinm.cminor9@gmail.com>
2018-07-22 10:42:02 +0900

    Add eslint loader

--- 03_webpack-4-tutorial/webpack.config.js
+++ 03_webpack-4-tutorial/webpack.config.js
@@ -20,6 +20,12 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
       }
     ]
   }
