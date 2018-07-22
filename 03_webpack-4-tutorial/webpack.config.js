58a168685da419e268f0db312ee7b13486a98ca5
yinm <yinm.cminor9@gmail.com>
2018-07-22 11:27:26 +0900

    Add mode division

--- 03_webpack-4-tutorial/webpack.config.js
+++ 03_webpack-4-tutorial/webpack.config.js
@@ -1,38 +1,56 @@
 const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const path = require('path')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/js/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'public/js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [['env', { modules: false }]]
+module.exports = (env, argv) => {
+  const IS_DEVELOPMENT = argv.mode === 'development'
+
+  return {
+    entry: './src/js/app.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, 'public/js')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [['env', { modules: false }]]
+              }
             }
-          }
+          ]
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery'
+      })
+    ],
+    devtool: IS_DEVELOPMENT ? 'source-map' : 'none',
+    optimization: {
+      minimizer: IS_DEVELOPMENT
+        ? []
+        : [
+          new UglifyJSPlugin({
+            uglifyOptions: {
+              compress: {
+                drop_console: true
+              }
+            }
+          })
         ]
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery'
-    })
-  ]
+    }
+  }
 }
