c5ebffd48c058735eb77efda50c482d78d087f0f
yinm <yinm.cminor9@gmail.com>
2018-08-31 09:23:49 +0900

    Add webpack.config.js

---
+++ 07_tutorial-sass-image-url/webpack.config.js
@@ -0,0 +1,36 @@
+const MODE = 'development'
+
+const enabledSourceMap = (MODE === 'development')
+
+module.exports = {
+  mode: MODE,
+
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: true,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+        loader: 'url-loader'
+      }
+    ],
+  },
+}
